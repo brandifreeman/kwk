f55fb13204741c56141c079022a79b490daa922d
Brandi Freeman <brandifreeman@Brandis-MacBook-Pro.local>
2019-05-17 17:36:35 -0500

    testing

---
+++ Practice Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice Git
+//
+//  Created by Brandi Freeman on 5/17/19.
+//  Copyright © 2019 Brandi Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
